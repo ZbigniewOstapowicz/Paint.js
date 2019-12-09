8a738304b7f3ae274a83f8c03364d4a61ad69aac
zbynioo85 <zbynioo85@gmail.com>
2019-12-09 23:31:06 +0100

    ad js and some style

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
+const canvas1= document.querySelector('[data-js="canvas1"]');
+const canvas2= document.querySelector('[data-js="canvas2"]');
+const ctx1=canvas1.getContext('2d');
+const ctx2=canvas2.getContext('2d');
+const content= document.querySelector('[data-js="content"]');
+const contentSize=getComputedStyle(content);
+canvas1.width=parseInt(contentSize.getPropertyValue('width'));
+canvas1.height=parseInt(contentSize.getPropertyValue('height'));
+canvas2.width=parseInt(contentSize.getPropertyValue('width'));
+canvas2.height=parseInt(contentSize.getPropertyValue('height'));
+
 
