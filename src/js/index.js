22333df117aa5b9408dc7af0e1a9f809ece2b8bc
zbynioo85 <zbynioo85@gmail.com>
2019-12-12 23:45:55 +0100

    add draw and buuton

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import Paint from './paint';
 const canvas1= document.querySelector('[data-js="canvas1"]');
 const canvas2= document.querySelector('[data-js="canvas2"]');
 const ctx1=canvas1.getContext('2d');
@@ -8,6 +9,16 @@ canvas1.width=parseInt(contentSize.getPropertyValue('width'));
 canvas1.height=parseInt(contentSize.getPropertyValue('height'));
 canvas2.width=parseInt(contentSize.getPropertyValue('width'));
 canvas2.height=parseInt(contentSize.getPropertyValue('height'));
-console.log(canvas2.width, canvas2.height);
 
+window.addEventListener('resize',()=>{
+canvas1.width=parseInt(contentSize.getPropertyValue('width'));
+canvas1.height=parseInt(contentSize.getPropertyValue('height'));
+canvas2.width=parseInt(contentSize.getPropertyValue('width'));
+canvas2.height=parseInt(contentSize.getPropertyValue('height'));
+});
+
+
+const CanPaint=new Paint(canvas1,ctx1,canvas2,ctx2);
+CanPaint;
+// CanPaint.draw();
 
---
+++ src/js/paint.js
@@ -0,0 +1,172 @@
+class Paint {
+  constructor(canvas, ctx, canvas2, ctx2,) {
+    this.canvas = canvas;
+    this.canvas2 = canvas2;
+    this.ctx = ctx;
+    this.ctx2 = ctx2;
+    this.rect = null;
+    this.controls();
+    this.setupInit();
+    this.handelControls();
+  }
+ setupInit(){
+    this.ctx.lineJoin = "round";
+    this.ctx.lineCap = "round";
+    this.lineWidthEle.value=3;
+    this.ctx.lineWidth=this.lineWidthEle.value;
+    this.sizeElemVal.innerText = this.lineWidthEle.value;
+ }
+
+
+  controls(){
+        this.lineWidthEle = document.querySelector('.paint-size');
+        this.sizeElemVal = document.querySelector('.paint-size-val');
+        this.sizeElemVal.innerText = this.lineWidthEle.value;
+        this.lineColorEle = document.querySelector('#color');
+  }
+  handelControls( ){
+    this.lineWidthEle.addEventListener('input', (e)=>{
+      // this.ctx.lineWidth=e.target.value;
+      this.sizeElemVal.innerText=e.target.value;
+    })
+    this.lineWidthEle.addEventListener('change', (e)=>{
+      
+      this.ctx.lineWidth=e.target.value;
+    })
+    this.lineColorEle.addEventListener('change', (e)=>{
+      this.ctx.strokeStyle=e.target.value;
+    })
+    window.onresize=()=>{
+      this.setupInit();
+    };
+    this.buttons=document.querySelectorAll('.btn');
+    this.buttons.forEach((button)=>{
+      button.addEventListener('click',(e)=>{
+        console.log(e);
+        if(button.classList.contains('draw')){
+          button.classList.add('active');
+          this.draw();
+        }else if(button.classList.contains('drawLine')){
+          button.classList.add('active');
+          this.drawLine();
+        }
+
+      for(const ele of this.buttons){
+        if(ele!=e.currentTarget){
+          ele.classList.remove('active');
+
+        }
+      }
+      })
+    })
+  }
+
+  positionStart(e) {
+    this.rect = {};
+    this.rect.x0 = e.offsetX;
+    this.rect.y0 = e.offsetY;
+  }
+  positionEnd(e) {
+    this.rect.x1 = e.offsetX;
+    this.rect.y1 = e.offsetY;
+  }
+  positionMobileStart(e) {
+    this.rect = {};
+    this.rect.correction = this.canvas.getBoundingClientRect();
+    this.rect.x0 = e.touches[0].pageX - this.rect.correction.x;
+    this.rect.y0 = e.touches[0].pageY - this.rect.correction.y;
+  }
+  positionMobileEnd(e) {
+    this.rect.x1 = e.touches[0].pageX - this.rect.correction.x;
+    this.rect.y1 = e.touches[0].pageY - this.rect.correction.y;
+  }
+  draw() {
+    this.canvas.ontouchstart = ev => {
+      this.positionMobileStart(ev);
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.rect.x0, this.rect.y0);
+    };
+    this.canvas.onmousedown = ev => {
+      this.positionStart(ev);
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.rect.x0, this.rect.y0);
+    };
+    this.canvas.ontouchmove = ev => {
+      ev.preventDefault();
+      this.positionMobileEnd(ev);
+      if (this.rect) {
+        this.positionMobileEnd(ev);
+        this.ctx.lineTo(this.rect.x1, this.rect.y1);
+        this.ctx.stroke();
+      }
+    };
+    this.canvas.onmousemove = ev => {
+      if (this.rect) {
+        this.positionEnd(ev);
+        this.ctx.lineTo(this.rect.x1, this.rect.y1);
+        this.ctx.stroke();
+      }
+    };
+    this.canvas.onmouseup = ev => {
+      this.ctx2.drawImage(this.canvas, 0, 0);
+      this.rect = null;
+    };
+    this.canvas.ontouchend = ev => {
+      this.ctx2.drawImage(this.canvas, 0, 0);
+      this.rect = null;
+    };
+  }
+  drawLine() {
+     this.canvas.ontouchstart = ev => {
+      this.positionMobileStart(ev);
+    };
+    this.canvas.onmousedown = ev => {
+      this.positionStart(ev)
+      // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      // this.ctx.beginPath();
+    };
+
+    this.canvas.ontouchmove = ev => {
+      ev.preventDefault();
+      if (this.rect) {
+        this.positionMobileEnd(ev)
+         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.beginPath();
+        // this.ctx.stroke(this.rect.x0, this.rect.y0,
+        // this.rect.x1,this.rect.y1);
+        this.ctx.moveTo(this.rect.x0, this.rect.y0);
+        this.ctx.lineTo(this.rect.x1, this.rect.y1);
+        this.ctx.closePath();
+        this.ctx.stroke();
+      }
+    };
+
+    this.canvas.onmousemove = ev => {
+      if (this.rect) {
+       this.positionEnd(ev)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.beginPath();
+        // this.ctx.stroke(this.rect.x0, this.rect.y0,
+        // this.rect.x1,this.rect.y1);
+        this.ctx.moveTo(this.rect.x0, this.rect.y0);
+        this.ctx.lineTo(this.rect.x1, this.rect.y1);
+        this.ctx.closePath();
+        this.ctx.stroke();
+      }
+    };
+
+    this.canvas.ontouchend = ev => {
+      this.ctx2.drawImage(this.canvas, 0, 0);
+      this.rect = null;
+    };
+
+    this.canvas.onmouseup = ev => {
+      // this.ctx.strokeRect(this.rect.x0, this.rect.y0,
+      //     this.rect.x1 - this.rect.x0, this.rect.y1 - this.rect.y0);
+      //   this.ctx.stroke();
+      this.ctx2.drawImage(this.canvas, 0, 0);
+      this.rect = null;
+    };
+  }
+}
+export default Paint;
